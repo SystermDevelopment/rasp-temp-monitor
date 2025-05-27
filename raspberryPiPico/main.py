931886e075a1945b02a268ce9321c2b79caa4473
c_yara <c_yara@technosphere.co.jp>
2025-05-27 13:31:41 +0900

    通信が途切れてしまう問題を解消

--- raspberryPiPico/main.py
+++ raspberryPiPico/main.py
@@ -2,11 +2,12 @@ import network
 import socket
 import json
 import time
+import machine
 from machine import Pin, I2C
 import bme280
 import config  # Wi-Fi設定を別ファイルで管理
 
-# Wi-Fi接続
+# Wi-Fi接続設定
 ssid = config.SSID
 password = config.PASSWORD
 
@@ -16,23 +17,49 @@ wlan.connect(ssid, password)
 
 while not wlan.isconnected():
     print('Wi-Fi接続中...')
-    time.sleep(10)
+    time.sleep(5)
 print('接続完了:', wlan.ifconfig())
 
-# センサー準備
+# センサー初期化
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))
 sensor = bme280.BME280(i2c=i2c, address=0x76)
 
-# Webサーバー起動
+# ソケット初期化
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 s.listen(1)
 s.settimeout(1)
-print('Pico WHサーバー起動中', addr)
+print('Pico WHサーバー起動中:', addr)
+
+# Wi-Fi再接続関数
+def reconnect_wifi():
+    if not wlan.isconnected():
+        print('Wi-Fi再接続中...')
+        wlan.disconnect()
+        wlan.connect(ssid, password)
+        timeout = 20
+        while not wlan.isconnected() and timeout > 0:
+            time.sleep(1)
+            timeout -= 1
+        if wlan.isconnected():
+            print('Wi-Fi再接続成功:', wlan.ifconfig())
+        else:
+            print('Wi-Fi再接続失敗')
+
+# メインループ
+last_wifi_check = time.time()
 
 while True:
+    current_time = time.time()
+
+    # 5分ごとのWi-Fiチェック
+    if current_time - last_wifi_check > 300:
+        reconnect_wifi()
+        last_wifi_check = current_time
+
+    cl = None
     try:
         cl, addr = s.accept()
         request = cl.recv(1024)
@@ -46,15 +73,15 @@ while True:
 
         cl.send('HTTP/1.0 200 OK\r\nContent-type: application/json\r\nAccess-Control-Allow-Origin: *\r\n\r\n')
         cl.send(response_data)
-        cl.close()
 
     except OSError as e:
-        if e.args[0] == 110:  # ETIMEDOUTの場合は無視する
-            pass
-        else:
+        if e.args[0] != 110:
             print('予期せぬエラー:', e)
+            machine.reset()
 
-    # 負荷軽減のスリープ
+    finally:
+        if cl:
+            cl.close()
+
     time.sleep(0.5)
-
 
